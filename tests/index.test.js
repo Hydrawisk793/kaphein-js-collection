572e86436e3249a243ffcbbd028f56d05479fd1a
Hydrawisk793 <hyw793@gmail.com>
2021-03-20 16:56:05 +0100

    test: remove imported module variables of test suites in index.test.js.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,8 @@
-const ArrayQueueTest = require("./suites/array-queue.test");
-const ListQueueTest = require("./suites/list-queue.test");
-const RbTreeSetTest = require("./suites/rb-tree-set.test");
-const RbTreeMapTest = require("./suites/rb-tree-map.test");
-const StringKeyTrieTest = require("./suites/string-key-trie.test");
-
 describe("kaphein-js-collection", function ()
 {
-    describe("ArrayQueue", ArrayQueueTest.bind(this));
-    describe("ListQueue", ListQueueTest.bind(this));
-    describe("RbTreeSet", RbTreeSetTest.bind(this));
-    describe("RbTreeMap", RbTreeMapTest.bind(this));
-    describe("StringKeyTrie", StringKeyTrieTest.bind(this));
+    describe("ArrayQueue", require("./suites/array-queue.test").bind(this));
+    describe("ListQueue", require("./suites/list-queue.test").bind(this));
+    describe("RbTreeSet", require("./suites/rb-tree-set.test").bind(this));
+    describe("RbTreeMap", require("./suites/rb-tree-map.test").bind(this));
+    describe("StringKeyTrie", require("./suites/string-key-trie.test").bind(this));
 });
